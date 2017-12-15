fc780345081e008a6eb88aa3acab4121c802fa08
Ysee Monnier <yseemonnier@gmail.com>
2017-12-15 16:17:27 +0100

    Add plugin to app sample

--- android-plugin/app/www/js/index.js
+++ android-plugin/app/www/js/index.js
@@ -17,30 +17,46 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+ // Application Constructor
+ initialize: function() {
+  document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+ },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+ // deviceready Event Handler
+ //
+ // Bind any cordova events here. Common events are:
+ // 'pause', 'resume', etc.
+ onDeviceReady: function() {
+  this.receivedEvent('deviceready');
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  alertplugin.showAlert(
+   'Super Title',
+   'Super description!',
+   function(msg) {
+    document
+     .getElementById('deviceready')
+     .querySelector('.received')
+     .innerHTML = msg;
+   },
+   function(err) {
+    document
+     .getElementById('deviceready')
+     .innerHTML = '<p class="event received">' + err + '</p>';
+   }
+  );
+ },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+ // Update DOM on a Received Event
+ receivedEvent: function(id) {
+  var parentElement = document.getElementById(id);
+  var listeningElement = parentElement.querySelector('.listening');
+  var receivedElement = parentElement.querySelector('.received');
 
-        console.log('Received Event: ' + id);
-    }
+  listeningElement.setAttribute('style', 'display:none;');
+  receivedElement.setAttribute('style', 'display:block;');
+
+  console.log('Received Event: ' + id);
+ }
 };
 
 app.initialize();
