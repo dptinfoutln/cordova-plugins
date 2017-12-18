3ed9c6c1b1c182626c66f85f7076faf85a6ca297
Ysee Monnier <yseemonnier@gmail.com>
2017-12-18 18:04:21 +0100

    add google maps with missing parts

---
+++ google-maps/www/js/maps.js
@@ -0,0 +1,13 @@
+var map;
+document.addEventListener("deviceready", function() {
+ // TODO: Create google maps instance
+
+ // TODO: Add google maps listener 'plugin.google.maps.event.MAP_READY'
+ // with onMapReady callback function.
+}, false);
+
+// Callback when the maps is ready.
+function onMapReady() {
+ console.log("OnMapReady");
+ // TODO: create your markers, polylines, polygons...
+}
