4912f87ea349d93b12cb94c6060d993cb0605352
Stephen Bellanger <stephenbellanger@gmail.com>
2017-12-29 17:48:02 +0100

    Add multiple markers solution

--- Google-maps/www/js/maps.js
+++ Google-maps/www/js/maps.js
@@ -1,6 +1,29 @@
 var map;
+var coordinates = []
+var URL = "https://demo8699198.mockable.io/restaurants"
+
 document.addEventListener("deviceready", function() {
   var div = document.getElementById("map_canvas");
+
+  $.ajax({
+      url : URL,
+      type : "GET",
+      data: {
+      format: 'json'
+      },
+      success : function(json){
+          var i;
+          for(i = 0; i < json.positions.length; i++){
+            coordinates.push({latitude: json.positions[i].geometry.coordinates[0],
+              longitude: json.positions[i].geometry.coordinates[1],
+              title: json.positions[i].properties.name,
+              type: json.positions[i].type});
+          }
+      },
+      error : function(resultat, statut, erreur){
+        alert("Error : " + statut);
+      }
+   });
 
   // Initialize the map view
   map = plugin.google.maps.Map.getMap(div);
@@ -14,15 +37,32 @@ function onMapReady() {
 
   // Move to the position with animation
   map.animateCamera({
-    target: {lat: 48.862725, lng: 2.287592000000018},
-    zoom: 17,
+    target: {lat: 43.124228, lng: 5.927999999999997},
+    zoom: 8,
     tilt: 60,
     bearing: 140,
     duration: 5000
   }, function() {
 
+    var i;
+    for(i = 0; i < coordinates.length; i++){
+
+      map.addMarker({
+        position: {lat: coordinates[i].latitude, lng: coordinates[i].longitude},
+        title: coordinates[i].title,
+        snippet: coordinates[i].type,
+        animation: plugin.google.maps.Animation.BOUNCE
+      }, function(marker) {
+
+        // Show the info window
+        //marker.showInfoWindow();
+
+        });
+
+    }
+
     // Add a maker
-    map.addMarker({
+    /*map.addMarker({
       position: {lat: 48.862725, lng: 2.287592000000018},
       title: "Universite De Toulon - La Garde" +
              "\nBienvenu a l'Universite !",
@@ -37,7 +77,6 @@ function onMapReady() {
       marker.on(plugin.google.maps.event.INFO_CLICK, function() {
 
       });
-    });
+    });*/
   });
-
 }
